b1d1dd55c11f6d2ca811892066ecefaec62f3cfd
Pratiksha0107 <31535332+Pratiksha0107@users.noreply.github.com>
2017-09-01 17:50:57 +0530

    Update TC_newUser.java

--- src/test/java/MavenProj/homePage/TC_newUser.java
+++ src/test/java/MavenProj/homePage/TC_newUser.java
@@ -64,6 +64,7 @@ public class TC_newUser extends TestBase{
 	{
 		homepage = new HomePage(driver);
                 log.info("Pratiksha Testing GitHub Branch");
+		 log.info("PTesting change direct in MAster");
 		log.info("***************** Create new user ************************");
 		homepage.createAccount("amoljadhao1@gmail.com");
 		homepage.personalInformation("amolo1", "jadhavo1", "P@ssw0rd", "amol", "jadhav","address", "city", "12332", "8806468985");
